3f7c9d90b61ef629e8c62dbbd19437f4b4b621c0
Max Ali <mohseni716015@gmail.com>
2024-07-16 23:37:26 +0200

    nya uppdatering

--- src/Components/home/Hero/Hero.jsx
+++ src/Components/home/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <section className='hero'>
         <div className='container'>
           <div className='row'>
-            <h1> Välokomen till MaxAi & Innovation </h1> 
+            <h1> Välkomen till MaxAi & Innovation </h1> 
             <h3> En guide och vägledning för en smartare framtid</h3>
             <div className='button'>
               <button className='primary-btn'>
